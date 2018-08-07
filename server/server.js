3ec7065d5a96ebb971f6abf0a025b0e8ea6cabbe
Amrender-singh <amrendersingh1995@gmail.com>
2018-08-08 00:17:38 +0530

    Send message to only people in same room

--- server/server.js
+++ server/server.js
@@ -36,8 +36,11 @@ io.on('connection', (socket)=>{
     });
 
     socket.on('createMessage', (message, callback)=>{
-         //it is used to emmit an event to all the connections. // first arguemnt event name second argurment data.
-        io.emit('newMessage',generateMessage(message.from, message.text));
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.text)){
+            //it is used to emmit an event to all the connections. // first arguemnt event name second argurment data.
+            io.to(user.room).emit('newMessage',generateMessage(user.name, message.text));
+        }
         callback();
         /* socket.broadcast.emit('newMessage',{
             from : message.from,
@@ -46,7 +49,10 @@ io.on('connection', (socket)=>{
         }); */
     });
     socket.on('createLocationMessage',(coords)=>{
-        io.emit('newLocationMessage',generateLocationMessage("user", coords.latitude, coords.longitude));
+        var user = users.getUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name , coords.latitude, coords.longitude));
+        }
 
     });
     socket.on('disconnect', ()=>{
